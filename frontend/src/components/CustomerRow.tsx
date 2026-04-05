0a5fa6905185aba422cb10bcc2d3021353a51c64
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2026-04-05 20:42:49 +0100

    Add click-to-copy to customer ID

--- frontend/src/components/CustomerRow.tsx
+++ frontend/src/components/CustomerRow.tsx
@@ -2,6 +2,8 @@ import { GoTrash } from "react-icons/go";
 import type { CustomerItem } from "../Types";
 import type { UUIDTypes } from "uuid";
 import "../styles/components/CustomerRow.css";
+import { useState } from "react";
+import { set } from "zod";
 
 export default function CustomerRow(
     { customer, deleteCustomerHandler } : 
@@ -10,9 +12,27 @@ export default function CustomerRow(
         deleteCustomerHandler: (id: UUIDTypes) => void,
     }
 ) {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = async () => {
+        try {
+            await navigator.clipboard.writeText(customer.id.toString());
+            setCopied(true);
+            setTimeout(() => setCopied(false), 1500); // Reset after 1.5 seconds
+        } catch (err) {
+            console.error("Failed to copy!", err);
+        }
+    };
+
     return (
         <div className="customer-table-row">
-            <span>{customer.id}</span>
+            <span 
+                className="copyable-customer-id" 
+                onClick={handleCopy}
+                title="Click to copy ID"
+            >
+                {copied ? "Copied to clipboard!" : customer.id}
+            </span>
             <span>{customer.first_name}</span>
             <span>{customer.second_name}</span>
             <span>{customer.email}</span>
